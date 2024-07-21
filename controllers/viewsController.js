f2bc109a184636ae04448d1cc9e66fbdcdd3821f
heymitali <mitalisinghnaruka@gmail.com>
2024-07-22 00:18:29 +0530

    implemented account features - edit name, edit password

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -64,3 +64,28 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
     user: updatedUser,
   });
 });
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
+});
